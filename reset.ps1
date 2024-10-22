82295eb6915b9b153c318a22fd37a3ea7a69f12f
Socrates5068 <marco.5068.2@gmail.com>
2024-10-21 23:07:49 -0400

    update reset

--- reset.ps1
+++ reset.ps1
@@ -6,4 +6,4 @@ docker compose down -v
 
 # Construir, (re)crear, iniciar y conectar a contenedores en segundo plano.
 Write-Output "Ejecutando docker compose up -d --build"
-docker compose up -d --build
+docker compose up -d
